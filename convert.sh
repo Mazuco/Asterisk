0411b6967314c5cef69c8e7220372746506f996d
Vitor Mazuco <vitor.mazuco@gmail.com>
2022-09-22 10:03:30 -0300

    Add files via upload

--- convert.sh
+++ convert.sh
@@ -1,6 +1,58 @@
 #!/bin/bash
+# convert.sh
 
-apt install sox -y
+set -e
+
+YUM_PACKAGE_NAME="sox"
+DEB_PACKAGE_NAME="sox"
+
+ if cat /etc/*release | grep ^NAME | grep CentOS; then
+    echo "==============================================="
+    echo "Installing packages $YUM_PACKAGE_NAME on CentOS"
+    echo "==============================================="
+    dnf install -y $YUM_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Red; then
+    echo "==============================================="
+    echo "Installing packages $YUM_PACKAGE_NAME on RedHat"
+    echo "==============================================="
+    dnf install -y $YUM_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Rocky; then
+    echo "==============================================="
+    echo "Installing packages $YUM_PACKAGE_NAME on RedHat"
+    echo "==============================================="
+    dnf install -y $YUM_PACKAGE_NAME
+  elif cat /etc/*release | grep ^NAME | grep Alma; then
+    echo "==============================================="
+    echo "Installing packages $YUM_PACKAGE_NAME on RedHat"
+    echo "==============================================="
+    dnf install -y $YUM_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Fedora; then
+    echo "================================================"
+    echo "Installing packages $YUM_PACKAGE_NAME on Fedorea"
+    echo "================================================"
+    dnf install -y $YUM_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Ubuntu; then
+    echo "==============================================="
+    echo "Installing packages $DEB_PACKAGE_NAME on Ubuntu"
+    echo "==============================================="
+    apt update
+    apt install -y $DEB_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Debian ; then
+    echo "==============================================="
+    echo "Installing packages $DEB_PACKAGE_NAME on Debian"
+    echo "==============================================="
+    apt update
+    apt install -y $DEB_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Mint ; then
+    echo "============================================="
+    echo "Installing packages $DEB_PACKAGE_NAME on Mint"
+    echo "============================================="
+    apt update
+    apt install -y $DEB_PACKAGE_NAME
+ else
+    echo "OS NOT DETECTED, couldn't install package $PACKAGE"
+    exit 1;
+ fi
 
 for a in $(find . -name '*.sln16'); do
   sox -t raw -e signed-integer -b 16 -c 1 -r 16k $a -t gsm -r 8k `echo $a|sed "s/.sln16/.gsm/"`;
